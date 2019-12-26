c29b8a57822b54e3277bac73effb510bf273dd04
dbigge <32107904+dbigge@users.noreply.github.com>
2019-12-26 10:29:28 -0800

    Add files via upload

---
+++ .github/workflows/Email-change-dbigge.py
@@ -0,0 +1,18 @@
+﻿# $language = "python"
+# $interface = "1.0"
+
+# This automatically generated script may need to be
+# edited in order to work correctly.
+
+def Main():
+	crt.Screen.Synchronous = True
+	crt.Screen.Send("cd nodetracker" + chr(13))
+	crt.Screen.WaitForString(chr(27) + "]0;devops@zen-1: " + chr(126) + "/nodetracker" + chr(7) + "devops@zen-1:" + chr(126) + "/nodetracker$ ")
+	crt.Screen.Send("sed -i 's/david./d/' ./config/config.json" + chr(13))
+	crt.Screen.WaitForString(chr(27) + "]0;devops@zen-1: " + chr(126) + "/nodetracker" + chr(7) + "devops@zen-1:" + chr(126) + "/nodetracker$ ")
+	crt.Screen.Send("node app.js" + chr(13))
+	crt.Screen.Send(chr(3) + "cat .config/c" + chr(8) + chr(8) + chr(8) + chr(8) + chr(8) + chr(8) + chr(8) + "/" + chr(8) + chr(8) + "/config/config.js" + chr(8) + "on" + chr(13))
+	crt.Screen.WaitForString(chr(27) + "]0;devops@zen-1: " + chr(126) + "/nodetracker" + chr(7) + "devops@zen-1:" + chr(126) + "/nodetracker$ ")
+	crt.Screen.Send(chr(27) + "[A" + chr(27) + "[D" + chr(27) + "[Ds" + chr(13))
+
+Main()
---
+++ .github/workflows/email-update-config.py
@@ -0,0 +1,16 @@
+﻿# $language = "python"
+# $interface = "1.0"
+
+# This automatically generated script may need to be
+# edited in order to work correctly.
+
+def Main():
+	crt.Screen.Synchronous = True
+	crt.Screen.Send("cd nodetracker" + chr(13))
+	crt.Screen.WaitForString(chr(27) + "]0;devops@zen-2: " + chr(126) + "/nodetracker" + chr(7) + "devops@zen-2:" + chr(126) + "/nodetracker$ ")
+	crt.Screen.Send("sed -k " + chr(8) + chr(8) + "i 's/david/d/' ./config/config.json" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D" + chr(27) + "[D." + chr(13))
+	crt.Screen.WaitForString(chr(27) + "]0;devops@zen-2: " + chr(126) + "/nodetracker" + chr(7) + "devops@zen-2:" + chr(126) + "/nodetracker$ ")
+	crt.Screen.Send("node app.js" + chr(13))
+	crt.Screen.Send(chr(3))
+
+Main()
